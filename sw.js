4bf045435baf3b62b3cfc55dc119af6f719d7b47
AquaPirot <acaglobus@gmail.com>
2026-06-08 22:51:20 +0200

    fix: bump SW cache to ph-v3 to pick up print background fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ph-v2';
+const CACHE = 'ph-v3';
 const SHELL = ['./','./index.html','./manifest.json','./sw.js','./icons/icon.svg','./icons/icon-maskable.svg'];
 
 self.addEventListener('install', e => {
